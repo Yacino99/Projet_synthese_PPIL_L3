c5c9c270ff8203a411c623094f3e393b8d7654ef
RuanHenry <Henry-R@hotmail.fr>
2022-02-14 17:32:50 +0100

    Ajout des .cpp

---
+++ src/Croix.cpp
@@ -0,0 +1,56 @@
+//
+// Created by Anwender on 14/02/2022.
+//
+
+#include "Croix.h"
+
+void Croix::dessiner(Socket *ss) const
+{
+
+    ostringstream oss;
+    oss << (string)*this;
+    string requete = oss.str();
+    ss->sendMessage(requete.c_str());
+
+}
+
+double Croix::calculerAire() const {
+    return 0;
+}
+
+Vecteur2D Croix::getCentreSymetrie() const {
+    double x = (a.x+b.x)/2;
+    double y = (a.y+b.y)/2;
+    return Vecteur2D(x,y);
+}
+
+Croix::operator string() const
+{
+    ostringstream os;
+
+    os << "croix ( " << a.x << " , " <<
+       a.y << " , " << b.x << " , " << b.y << " )" << this->couleur;
+
+
+    return os.str();
+}
+
+void Croix::translation(const Vecteur2D &u)
+{
+    a = a + u;
+    b = b + u;
+}
+
+void Croix::homothetie(const Vecteur2D &u, double k) {
+    a =  (a - u)*k  + u;
+    b =  (b - u)*k  + u;
+}
+
+//Vecteur OB’ = R * (vecteur OB – vecteur OC) + vecteur OC
+void Croix::rotation(const Vecteur2D &u, const double angle) {
+    Matrice22 M(Vecteur2D(cos(angle),-sin(angle)) , Vecteur2D(sin(angle), cos(angle)));
+    b =  M * (b - u) + u ;
+    a =  M * (a - u) + u ;
+}
+
+
---
+++ src/Rond.cpp
@@ -0,0 +1,54 @@
+//
+// Created by Anwender on 14/02/2022.
+//
+
+#include "Rond.h"
+#include "Matrice22.h"
+
+void Rond::dessiner(Socket *ss) const
+{
+    ostringstream oss;
+    oss << (string)*this;
+    string requete = oss.str();
+    ss->sendMessage(requete.c_str());
+}
+
+void Rond::translation(const Vecteur2D& u)
+{
+    v = v + u;
+}
+
+void Rond::homothetie(const Vecteur2D &u, double k) {
+    v = (v - u)*k  + u;
+    rayon *= k;
+}
+
+Rond::operator string() const
+{
+    ostringstream os;
+    os << "rond ( " << v.x << " , " << v.y << " , " << rayon <<" ) " << this->couleur;
+    return os.str();
+}
+
+double Rond::calculerAire() const {
+    return (rayon * rayon) * PI;
+}
+
+void Rond::rotation(const Vecteur2D &u, const double angle) {
+
+    double m11 = cos(angle);
+    double m12 = -sin(angle);
+    double m21 = sin(angle);
+    double m22 = cos(angle);
+
+    Matrice22 M(m11,m12,m21,m22);
+
+    v =  M * (v - u) + u ;
+
+}
+
+Vecteur2D Rond::getCentreSymetrie() const {
+    return v;
+}
+
+
