c92879384415de9dd0ff1809c5b3236b8baaaa8d
RuanHenry <Henry-R@hotmail.fr>
2022-02-27 18:09:23 +0100

    - DP Visitor pour les librairies graphiques (suppresion de l'ancienne méthode dessiner() + ajout de la nouvelle) - Suppresion du paramètre socket pour formes et ses enfants - Adaptation du nouveau modèle de dessine () dans chaque fichier (ExpertCOR, Formes ...)

---
+++ include/VisiteurLibrairieAwt.h
@@ -0,0 +1,22 @@
+//
+// Created by Anwender on 27/02/2022.
+//
+
+#ifndef PROJET_VISITEURLIBRAIRIEAWT_H
+#define PROJET_VISITEURLIBRAIRIEAWT_H
+
+#include "VisiteurLibrairie.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Socket.h"
+
+class VisiteurLibrairieAwt : public VisiteurLibrairie
+{
+
+public:
+    const void *visite(const Formes * forme, Socket * s) const;
+};
+
+
+#endif //PROJET_VISITEURLIBRAIRIEAWT_H
